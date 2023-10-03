5ad1615684d13732d3dd1fcb740b482e26df9630
jrcamelo <joaorafaelx@gmail.com>
2023-10-03 16:31:06 +0000

    Add cooldown and reacts

--- index.js
+++ index.js
@@ -1,26 +1,84 @@
 require('dotenv').config();
 const Discord = require('discord.js');
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS"] }); // Added GUILD_MESSAGE_REACTIONS intent
 
-const images = require('./rupert.json').images;
+let images = require('./rupert.json').images;
+let currentIndex = 0;
+const channelCooldowns = {};
+const serverCooldowns = {};
+const cooldownEmotes = [
+    '1158799845826498660', // JoyRupert
+    '1158800118745665616', // NapRupert
+    '1158799535716446288', // ScreamingRupert
+    '1158799858279399574', // UpsideDownRupert
+    '1158799865577492641', // WiseRupert
+    '1158800023392362647' // aMimirRupert
+];
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+// Shuffle multiple times because why not :)
+shuffleArray(images);
+shuffleArray(images);
+shuffleArray(images);
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('messageCreate', msg => {
+client.on('messageCreate', async msg => { 
     if (msg.author.bot) return;
 
+    const channelID = msg.channel.id;
+    const serverID = msg.guild.id;
+
     if (checkIfRupert(msg.content) || msg.mentions.has(client.user.id)) {
-        const currentTime = new Date().toLocaleString();
-        const serverID = msg.guild.id;
-        const channelID = msg.channel.id; 
-        console.log(`Rupert mentioned at ${currentTime} GMT+1 in server ${serverID}, channel ${channelID}: ${msg.content}`);
+        if (isOnCooldown(channelID, channelCooldowns, 2) || isOnCooldown(serverID, serverCooldowns, 5)) {
+            console.log("Cooldown activated");
+            const emoteID = cooldownEmotes[Math.floor(Math.random() * cooldownEmotes.length)];
+            await msg.react(emoteID);
+            return;
+        }
+
+        const currentTime = new Date().getTime();
+        console.log(`Rupert mentioned at ${new Date(currentTime).toLocaleString()} GMT+1 in server ${serverID}, channel ${channelID}: ${msg.content}`);
         
-        const randomImage = images[Math.floor(Math.random() * images.length)];
-        msg.channel.send(randomImage);
+        msg.channel.send(images[currentIndex]);
+        currentIndex++;
+        if (currentIndex >= images.length) {
+            shuffleArray(images);
+            currentIndex = 0;
+        }
+
+        if (!channelCooldowns[channelID]) {
+            channelCooldowns[channelID] = [];
+        }
+        channelCooldowns[channelID].push(currentTime);
+
+        if (!serverCooldowns[serverID]) {
+            serverCooldowns[serverID] = [];
+        }
+        serverCooldowns[serverID].push(currentTime);
     }
 });
+
+function isOnCooldown(id, cooldowns, limit) {
+    const currentTime = new Date().getTime();
+
+    if (!cooldowns[id]) {
+        cooldowns[id] = [];
+        return false;
+    }
+
+    cooldowns[id] = cooldowns[id].filter(timestamp => currentTime - timestamp < 60000);
+
+    return cooldowns[id].length >= limit;
+}
 
 function checkIfRupert(message) {
     const cleanedMessage = message.toLowerCase().replace(/[^\w\s]/g, '');
